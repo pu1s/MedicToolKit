49ec95e0d5cb2d8ea710a0611639ae85d31085f9
Горин Александр <pu1s@outlook.com>
2019-06-28 09:46:30 +0400

    *-----*

---
+++ Medic.Tools.Kit/Utils.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medic.Tools.Kit
+{
+    public struct Limits    
+    {
+        private double _lower;
+        private double _upper;
+
+        public double Lower
+        {
+            get
+            {
+                return _lower;
+            }
+        }
+
+        public double Upper
+        {
+            get
+            {
+                return _upper;
+            }
+        }
+        public double Range
+        {
+            get
+            {
+                return _upper - _lower;
+            }
+        }
+        public Limits(double lower, double upper)
+        {
+            _lower = lower;
+            _upper = upper;
+        }
+    }
+}
