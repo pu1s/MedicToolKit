dbf03dadf31d00c895d11631bdb306b1ab319454
Горин Александр <pu1s@outlook.com>
2019-06-28 09:36:39 +0400

    +++++++

---
+++ Medic.Tools.Kit/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medic.Tools.Kit
+{
+    public class Class1
+    {
+    }
+}
