5f622a5ae33759aa30873b2eec98fb2fe88b1cd9
Горин Александр <pu1s@outlook.com>
2019-07-06 18:56:21 +0400

    10

---
+++ Medic.Tools.Kit/UI/Controls/WatermarkTextBoxEx/LimitsErrorException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Medic.Tools.Kit.Utility
+{
+    public class LimitsErrorException : Exception
+    {
+        private LimitsErrorException()
+        {
+        }
+
+        public LimitsErrorException(string message) : base(message)
+        {
+        }
+
+        private string message;
+
+        public new string Message { get => message; private set => message = value; }
+    }
+
+}
