1ee4ed11615eadfe15b09728154a7c6410964f61
Горин Александр <pu1s@outlook.com>
2019-10-17 19:56:00 +0400

    new func

--- MedicalToolsKit/Medical Calculation.cs
+++ MedicalToolsKit/Medical Calculation.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MedicalToolsKit
 {
@@ -6,7 +7,7 @@ namespace MedicalToolsKit
     {
         private static float BodyMassIndexCalc(float wieght, float hight)
         {
-            if(wieght == 0 || hight == 0) throw new MedicCalcException(@"Argument is null!");
+            if (wieght == 0 || hight == 0) throw new MedicCalcException(@"Argument is null!");
             return wieght / (hight * hight);
         }
 
@@ -17,11 +18,66 @@ namespace MedicalToolsKit
 
     }
 
-    public class MedCalcArgsLimits
+    public struct MedCalcArgsLimit<T>
+    {
+        private T _lower;
+        private T _upper;
+
+        public MedCalcArgsLimit(T lower, T upper)
+        {
+            _lower = lower;
+            _upper = upper;
+        }
+
+        public T Upper => _upper;
+        public T Lower => _lower;
+    }
+
+    public struct MedCalcArgsLimitList<T>
+    {
+        private readonly List<MedCalcArgsLimit<T>> _limitsList;
+
+        public MedCalcArgsLimitList(List<MedCalcArgsLimit<T>> limitsList)
+        {
+            _limitsList = limitsList ?? throw new ArgumentNullException(nameof(limitsList));
+        }
+
+        public List<MedCalcArgsLimit<T>> LimitsList => _limitsList;
+
+        public override string ToString()
+        {
+			const string LOWER          = "Lower limit: ";
+			const string UPPER          = "Upper limit: ";
+			const string V_SEP          = "\n\r";
+			const string LCOUNT         = "Limits count: ";
+			const string H_SEP          = "======================";
+			string result               = string.Empty;
+            //
+            if(_limitsList == null) throw new ArgumentNullException(nameof(_limitsList));
+            //
+            result += LCOUNT + _limitsList.ToString() + V_SEP;
+            //
+            result += H_SEP + V_SEP;
+            //
+            foreach(var item in _limitsList)
+            {
+                result += LOWER + item.Lower;
+                result += UPPER + item.Upper;
+                result += V_SEP;
+            }
+            //
+            result += H_SEP + V_SEP;
+            return result;
+        }
+    }
+
+    #region 
+    //TODO: Do Not Impl
+    public class MedCalcArgsLimit : Exception
     {
 
     }
-
+    #endregion
 
     #region Medical Calculator Exception
 
@@ -29,7 +85,7 @@ namespace MedicalToolsKit
     {
         public new string Message { get; }
 
-        public MedicCalcException() :base()
+        public MedicCalcException() : base()
         {
             Message = string.Empty;
         }
@@ -40,4 +96,6 @@ namespace MedicalToolsKit
         }
     }
     #endregion
+
+
 }
