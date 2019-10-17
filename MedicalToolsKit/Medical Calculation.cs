1a26c20f232724ef124f8cfc3cc7a554f1734687
Горин Александр <pu1s@outlook.com>
2019-10-17 18:13:03 +0400

    New

---
+++ MedicalToolsKit/Medical Calculation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MedicalToolsKit
+{
+    public class MedCalc
+    {
+        private static float BodyMassIndexCalc(float wieght, float hight)
+        {
+
+            return wieght / (hight * hight);
+        }
+    }
+    public class MedicCalcException : Exception
+    {
+
+    }
+}
