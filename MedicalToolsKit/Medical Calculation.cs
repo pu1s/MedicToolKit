b848058f9dc0b966c4f8bf89c07ea705ebd18c88
Горин Александр <pu1s@outlook.com>
2019-10-17 18:30:39 +0400

    ++++

--- MedicalToolsKit/Medical Calculation.cs
+++ MedicalToolsKit/Medical Calculation.cs
@@ -6,12 +6,38 @@ namespace MedicalToolsKit
     {
         private static float BodyMassIndexCalc(float wieght, float hight)
         {
-
+            if(wieght == 0 || hight == 0) throw new MedicCalcException(@"Argument is null!");
             return wieght / (hight * hight);
         }
+
     }
-    public class MedicCalcException : Exception
+
+    public class MedCalcHelpers
     {
 
     }
+
+    public class MedCalcArgsLimits
+    {
+
+    }
+
+
+    #region Medical Calculator Exception
+
+    public class MedicCalcException : Exception
+    {
+        public new string Message { get; }
+
+        public MedicCalcException() :base()
+        {
+            Message = string.Empty;
+        }
+
+        public MedicCalcException(string message)
+        {
+            Message = message;
+        }
+    }
+    #endregion
 }
