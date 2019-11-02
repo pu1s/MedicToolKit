458950ae885dfedbe5d3816eed9dbc50c840c9bb
Горин Александр <pu1s@outlook.com>
2019-11-02 10:19:35 +0400

    ++++++

---
+++ MedicalToolsKit/Limits.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MedicalToolsKit
+{
+    public class Limits<T>
+    {
+        private T _lower;
+        private T _upper;
+
+        private Limits()
+        {
+            _lower = default;
+            _upper = default;
+        }
+
+        public Limits(T lower, T upper)
+        {
+            _lower = lower;
+            _upper = upper;
+        }
+
+        public bool IsTryLimits { get; }
+        public class LimitsRangeOutException : Exception
+        {
+            public LimitsRangeOutException(string message) : base(message)
+            {
+            }
+        }
+    }
+}
---
+++ MedicalToolsKit/MedicalCalculator.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace MedicalToolsKit
+{
+    public class MedicalCalculator
+    {
+        
+    }
+}
